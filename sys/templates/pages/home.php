50f536665d7be2f7ffbdbe1639ecac2ed611c781
Paul Smith <mail@paulsmith.site>
2017-07-30 21:28:50 +0100

    updates links and sidebar styles;

--- sys/templates/pages/home.php
+++ sys/templates/pages/home.php
@@ -35,7 +35,7 @@
             </footer>
           </article>
 
-          <article class="c-page-section" id="intro">
+          <article class="c-page-section" id="blog">
             <header class="c-page-section__header">
               <h2 class="c-page-section__heading"><?php perch_content('Blog section title'); ?></h2>
             </header>
@@ -58,10 +58,31 @@
             <div class="c-global-sidebar">
 
               <div class="c-global-sidebar__inner">
-                <h3 class="c-global-side__heading">Heading</h3>
-              </div>
 
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quo sunt odio molestiae, neque cumque praesentium? Autem perferendis, iusto sunt libero veritatis consectetur nesciunt est beatae! Consectetur, itaque illo odio.</p>
+                <article class="c-global-sidebar__section">
+                  <h3 class="c-global-sidebar__heading"><svg class="o-icon"><use xlink:href="#icon-open-book"></use></svg> Reading list</h3>
+                  <div class="c-global-sidebar__section-body">
+                    <?php
+                      perch_collection('Reading list', [
+                          'count' => 5,
+                          'sort'  => '_date',
+                          'sort-order' => 'DESC',
+                      ]);
+                    ?>
+                  </div>
+                  <div class="c-global-sidebar__section-footer">
+                    <p><a href="/readinglist/" class="c-link c-link--cta">View the full reading list<svg class="icon icon-small-arrow--r"><use xlink:href="#icon-small-arrow--r"></use></svg></a></p>
+                  </div>
+                </article>
+
+                <article class="c-global-sidebar__section">
+                  <h3 class="c-global-sidebar__heading"><svg class="o-icon"><use xlink:href="#icon-chat"></use></svg> Get involved</h3>
+                  <div class="c-global-sidebar__section-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </div>
+                </article>
+
+              </div>
 
             </div>
 
