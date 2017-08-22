1679f987e6db29035ed54e7158698017c45f824f
Paul Smith <mail@paulsmith.site>
2017-08-22 07:31:50 +0100

    adds viewport tag

--- sys/templates/layouts/global/head.php
+++ sys/templates/layouts/global/head.php
@@ -1,4 +1,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title><?php perch_layout_var('title') ?></title>
 
